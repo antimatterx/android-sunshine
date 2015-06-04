d75f7c0c42d03fe9f7f4eeeea948701ee2e983c5
CX <limchengxin@gmail.com>
2015-06-04 17:18:06 +0800

    Added Share button

--- app/src/main/java/com/cx/sunshine/DetailActivity.java
+++ app/src/main/java/com/cx/sunshine/DetailActivity.java
@@ -1,9 +1,8 @@
 package com.cx.sunshine;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 
 
@@ -13,14 +12,6 @@ public class DetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detail, menu);
-        return true;
     }
 
     @Override
@@ -38,4 +29,6 @@ public class DetailActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
